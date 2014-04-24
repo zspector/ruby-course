bc18b1bea31a0fc5f3dc6331df90dd996da8c9f0
Zachary Spector <zjspector@gmail.com>
2014-04-24 15:15:24 -0500

    Add self.histogram, self.calc_top_color, self.calc_bottom_color methods

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,21 @@
 class CarStats
   # TODO
+  def initialize
+    @cars = []
+  end
+
+  def self.histogram(array)
+    cars_hash = Hash.new(0)
+    array.each { |car| cars_hash[car.color] += 1 }
+    cars_hash
+  end
+
+  def self.calc_top_color(array)
+    histogram(array).max_by { |color, quantity| quantity }.first
+  end
+
+
+  def self.calc_bottom_color(array)
+    histogram(array).min_by { |color, quantity| quantity }.first
+  end
 end
