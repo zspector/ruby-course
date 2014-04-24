4d6874eead5b69c7a25da05402c087528e255207
Zachary Spector <zjspector@gmail.com>
2014-04-24 15:55:18 -0500

    Refactor with Proc

--- car_stats.rb
+++ car_stats.rb
@@ -1,8 +1,10 @@
 class CarStats
+  @@sort_quantity = Proc.new { |x, y| y }
   # TODO
   def initialize
     @cars = []
   end
+
 
   def self.histogram(array)
     cars_hash = Hash.new(0)
@@ -11,11 +13,12 @@ class CarStats
   end
 
   def self.calc_top_color(array)
-    histogram(array).max_by { |color, quantity| quantity }.first
+    histogram(array).max_by(&@@sort_quantity).first
   end
 
 
   def self.calc_bottom_color(array)
-    histogram(array).min_by { |color, quantity| quantity }.first
+    histogram(array).min_by(&@@sort_quantity).first
   end
 end
+
